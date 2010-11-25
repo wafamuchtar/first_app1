b4b1cd00c337cd8d21d863f2e3f5b96b1e47a59f
wafamuchtar <wafamuchtar@gmail.com>
2010-11-25 14:01:08 +0700

    wafa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp1::Application.config.secret_token = '253eb5fbf781ff703bd3562cd2181ff5bcc4a1d62fdff3b559e144d32b3eb8cc8d466056d4305aaea149c33d1580f502a347efc0c61f7a1ed3252828631f3591'
